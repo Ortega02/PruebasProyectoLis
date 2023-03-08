82964af375a0f0d010c2f737a7ff45b0bb6be861
Ortega02 <89029520+Ortega02@users.noreply.github.com>
2023-03-07 22:31:03 -0600

    Delete Proyecto-Lis directory

--- Proyecto-Lis/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-    define("CONTROLADOR_PRINCIPAL","Principal");
-    define ( "ACCION_PRINCIPAL" , "index" );
-?>
--- Proyecto-Lis/views/cliente/Borrar.php
+++
@@ -1,8 +0,0 @@
-<?php session_start(); 
-//require_once "views/cliente/Carrito.php";    
-
-header("Location: ".$_SERVER['HTTP_REFERER']."");
-unset($_SESSION['carrito']);
-
-
-
